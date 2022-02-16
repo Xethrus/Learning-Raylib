de52551ecb2fc3c1f94489f7d7be0a5c7c6131f4
Xethrus <80862366+Xethrus@users.noreply.github.com>
2022-02-15 19:50:51 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include "raylib.h"
 
+
+const int screenWidth = 800;
+const int screenHeight = 450;
+Vector2 startPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    Vector2 update(void) {
+    if(startPosition.y < screenHeight-30) { if (IsKeyDown(KEY_DOWN)) startPosition.y += 2.0f; }
+    if(startPosition.y > 30) { if (IsKeyDown(KEY_UP)) startPosition.y -= 2.0f; }
+    if(startPosition.x > 30) { if (IsKeyDown(KEY_LEFT)) startPosition.x -= 2.0f; }
+    if(startPosition.x < screenWidth-30) { if (IsKeyDown(KEY_RIGHT)) startPosition.x += 2.0f; }
+    return startPosition;
+    }
+void render(void) {
+    BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Move the ball with the arrow keys", startPosition.x-150, startPosition.y+50, 16, BLACK);
+        DrawCircle(startPosition.x, startPosition.y, 30, BLUE);  
+
+    EndDrawing(); 
+
+}
 int main(void)
 {
     // Initialization
@@ -18,22 +39,12 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        if(startPosition.y < screenHeight-30) { if (IsKeyDown(KEY_DOWN)) startPosition.y += 2.0f; }
-    
-        if(startPosition.y > 30) { if (IsKeyDown(KEY_UP)) startPosition.y -= 2.0f; }
-        if(startPosition.x > 30) { if (IsKeyDown(KEY_LEFT)) startPosition.x -= 2.0f; }
-        if(startPosition.x < screenWidth-30) { if (IsKeyDown(KEY_RIGHT)) startPosition.x += 2.0f; }
+        update();
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-            DrawText("Move the ball with the arrow keys", startPosition.x-150, startPosition.y+50, 16, BLACK);
-            DrawCircle(startPosition.x, startPosition.y, 30, BLUE);  
-
-        EndDrawing();
+        render();
         //----------------------------------------------------------------------------------
     }
 
