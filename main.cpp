dce0a9479c9904a4105a9d353b6e05b0669d7195
Xethrus <80862366+Xethrus@users.noreply.github.com>
2022-02-18 11:19:11 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,48 @@
 #include "raylib.h"
 #include <vector>
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 800;
-const int ballSize = 30;
+const float SCREEN_WIDTH = 800;
+const float SCREEN_HEIGHT = 800;
 std::vector<Vector2> squarePositions;
-Vector2 emptyVector;
+
 class Ball
 {
 public:
-    Vector2 ballPosition = {(float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2};
+    Vector2 ballPosition;
+    const int BALL_SIZE;
+    Ball(float x, float y, int size) : ballPosition{x, y}, BALL_SIZE{30} {};
 };
-void drawSquares(Vector2 square)
+
+void drawSquare(Vector2 square, Ball &ballP)
 {
 
-    DrawRectangle(square.x,square.y,ballSize,ballSize,BLACK);
+    DrawRectangle(square.x,square.y, ballP.BALL_SIZE, ballP.BALL_SIZE,BLACK);
 }
-void findsquarePositions()
+void iteratePositions()
 {
-    for (int i=0; i < squarePositions.size(); i++)
+    for (const auto& s : squarePositions)
     {
-        drawSquares(squarePositions[i]);
+        drawSquare(s, ball);
     }
 }
 Vector2 update(Ball &ballP)
 {
-    if (ballP.ballPosition.y < SCREEN_HEIGHT - ballSize)
+    if (ballP.ballPosition.y < SCREEN_HEIGHT - ballP.BALL_SIZE)
     {
         if (IsKeyDown(KEY_DOWN))
             ballP.ballPosition.y += 2.0f;
     }
-    if (ballP.ballPosition.y > ballSize)
+    if (ballP.ballPosition.y > ballP.BALL_SIZE)
     {
         if (IsKeyDown(KEY_UP))
             ballP.ballPosition.y -= 2.0f;
     }
-    if (ballP.ballPosition.x > ballSize)
+    if (ballP.ballPosition.x > ballP.BALL_SIZE)
     {
         if (IsKeyDown(KEY_LEFT))
             ballP.ballPosition.x -= 2.0f;
     }
-    if (ballP.ballPosition.x < SCREEN_WIDTH - ballSize)
+    if (ballP.ballPosition.x < SCREEN_WIDTH - ballP.BALL_SIZE)
     {
         if (IsKeyDown(KEY_RIGHT))
             ballP.ballPosition.x += 2.0f;
@@ -60,8 +62,8 @@ void render(Ball &ballP)
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText("Move the ball with the arrow keys", ballP.ballPosition.x - 150, ballP.ballPosition.y + 50, 16, BLACK);
-    DrawCircle(ballP.ballPosition.x, ballP.ballPosition.y, ballSize, BLUE);
-    findsquarePositions();
+    DrawCircle(ballP.ballPosition.x, ballP.ballPosition.y, ballP.BALL_SIZE, BLUE);
+    iteratePositions();
     EndDrawing();
 }
 
